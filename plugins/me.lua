74006855cf203d9977004081b698289c33f2259b
moody2020 <th3boss98@mail.ru>
2017-02-21 08:48:59 +0000

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -33,7 +33,7 @@ local function run(msg, matches)
 				else
 					rank = 'مجرد عضو 😹'
 				end
-				local text = '<b>اهـلا بـك ❤️ معلوماتك :</b>\n\n<b>الاسم الاول💡 :</b> <i>'..data.first_name_..'</i>\n\n<b>الاسم الثاني💡 :</b> <i>'..lastName..'</i>\n\n<b>المعرف💡:</b> '..username..'\n\n<b>الايدي💡 :</b> [ <code>'..data.id_..'</code> ]\n\n<b> ايدي الكروب💡 :</b> [ <code>'..arg.chat_id..'</code> ]\n\n<b>موقعك💡 :</b> <i>'..rank..'</i>\n\n<i>💡 مـطـور الـسـورس : @TH3BOSS</i>'
+				local text = '<b>❤️ اهـلا بـك معلوماتك :</b>\n\n<b>💡 الاسم الاول :</b> <i>'..data.first_name_..'</i>\n\n<b>💡 الاسم الثاني :</b> <i>'..lastName..'</i>\n\n<b>💡 المعرف:</b> '..username..'\n\n<b>💡 الايدي :</b> [ <code>'..data.id_..'</code> ]\n\n<b> 💡 ايدي الكروب :</b> [ <code>'..arg.chat_id..'</code> ]\n\n<b>💡 موقعك :</b> <i>'..rank..'</i>\n\n<i>💡 مـطـور الـسـورس : @TH3BOSS</i>'
 				tdcli.sendMessage(arg.chat_id, msg.id_, 1, text, 1, 'html')
 			end
 		end
