3737ff311bc032f20ccc48e197c4d5ed8d98e567
moody2020 <th3boss98@mail.ru>
2017-03-16 09:45:30 +0000

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -7,14 +7,15 @@ do local _ = {
     "msg-checks",
     "plugins",
     "tools",
-    "me"
+    "me",
+    "replay"
   },
   info_text = "📌¦ Welcome To My Dear\n\nTH3BOSS V12 \nFor More Information Subscribe To The Channel @llDEV1ll \n https://github.com/moody2020/TH3BOSS\n\n📌¦ Dev @TH3BOSS\n\n📌¦ Dev Bot @ll60Kllbot\n\n📌¦ Channel @llDEV1ll ",
   moderation = {
     data = "./data/moderation.json"
   },
   sudo_users = {
-    226861861,
+    226861861
   }
 }
 return _
