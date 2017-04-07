c6cca4911f53bda9ac08e7d3c94391e2aecc0933
moody2020 <th3boss98@mail.ru>
2017-04-07 14:29:50 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -8,14 +8,15 @@ do local _ = {
     "plugins",
     "tools",
     "me",
-    "addreplay"
+    "addreplay",
+    "deleall"
   },
-  info_text = "📌¦ Welcome To My Dear\n\nTH3BOSS V12 \nFor More Information Subscribe To The Channel @llDEV1ll \n https://github.com/moody2020/TH3BOSS\n\n📌¦ Dev @TH3BOSS\n\n📌¦ Dev Bot @ll60Kllbot\n\n📌¦ Channel @llDEV1ll ",
+  info_text = "📌¦ Welcome To My Dear\n\nTH3BOSS V13 \nFor More Information Subscribe To The Channel @llDEV1ll \n https://github.com/moody2020/TH3BOSS\n\n📌¦ Dev @TH3BOSS\n\n📌¦ Dev Bot @ll60Kllbot\n\n📌¦ Channel @llDEV1ll ",
   moderation = {
     data = "./data/moderation.json"
   },
   sudo_users = {
-    226861861
+    60809019
   }
 }
 return _
