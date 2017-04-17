1ef40534fee6e971cef84c214b67a664474cb2dd
zfu2 <usa13android@gmail.com>
2017-04-18 02:44:28 +0300

    Update MBC2.sh

--- MBC2.sh
+++ MBC2.sh
@@ -3,51 +3,22 @@
 cd $HOME/MBC2
 
 install() {
-     cd tg
-  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-  sudo apt-get install g++-4.7 -y c++-4.7 -y
-  sudo apt-get update
-  sudo apt-get upgrade
-  sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-  sudo apt-get install screen -y
-  sudo apt-get install tmux -y
-  sudo apt-get install libstdc++6 -y
-  sudo apt-get install lua-lgi -y
-  sudo apt-get install libnotify-dev -y
-  sudo service redis-server restart
-  wget https://valtman.name/files/telegram-cli-1222
-  mv telegram-cli-1222 tgcli
-  chmod +x tgcli
-  cd ..
-  chmod +x bot
-  chmod +x tg
-}
-
-function print_logo() {
- echo -e "\033[38;5;600m"
- echo -e "        "
- echo -e "        "
- echo -e "        "
- echo -e "    "
- echo -e "\n\e[36m"
-}
-
-function logo_play() {
-    declare -A txtlogo
-    seconds="0.010"
-    txtlogo[1]="   "
-    txtlogo[2]=""
-    txtlogo[3]=""
-    txtlogo[4]=""
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in seq 0 ${#txtlogo[$i]}; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
+	    cd tg
+		sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 c++-4.7
+		sudo apt-get update		
+        sudo apt-get upgrade
+		sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
+		sudo apt-get install lua-lgi
+		sudo apt-get install libnotify-dev
+		sudo apt-get install screen
+		sudo apt-get install tmux
+		wget https://valtman.name/files/telegram-cli-1222
+		mv telegram-cli-1222 tgcli
+		chmod +x tgcli
+		cd ..
+		chmod +x bot
+		chmod +x tg
 }
 
 if [ "$1" = "install" ]; then
@@ -58,20 +29,14 @@ if [ ! -f ./tg/tgcli ]; then
     echo "tg not found"
     echo "Run $0 install"
     exit 1
- fi
-
-
-   print_logo
+fi
    echo -e "\033[38;5;208m"
-   echo -e "     »»                       MBC2 BOT                             "
-   echo -e "     »»                       DEVLOPER @Cr7rC                             "
-   echo -e "     »»                       VIRSION 1                                   "
-   echo -e "     »»                       CH @C_R_4                             "
-   echo -e "     »»                       @Cr7rC                                     "
-   echo -e "\033[0;00m"
+   echo -e "     »,.»                       MBC2 BOT                             "
+   echo -e "     »,.»                       DEVLOPER @Cr7rC                             "
+   echo -e "     »,.»                       VIRSION 1                                   "
+   echo -e "     »,.»                       CH @C_R_4                             "
+   echo -e "     »,.»                       @Cr7rC                                     "
+   echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
-   logo_play
-   #sudo service redis-server restart
-   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
    ./tg/tgcli -s ./bot/bot.lua $@
 fi
